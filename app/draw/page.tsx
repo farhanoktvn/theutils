d308eee406bd1ea39fd1a3a99777369c1a34a790
Muhammad Farhan Oktavian <farhanoktvn@gmail.com>
2023-06-01 22:48:58 +0100

    disable prerender for /draw

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -1,12 +1,20 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 import { Excalidraw, FONT_FAMILY, THEME } from '@excalidraw/excalidraw';
 
 export default function TextPage() {
+  const [hasMounted, setHasMounted] = useState(false);
   const { theme } = useTheme();
 
   const currentTheme = theme === 'dark' ? THEME.DARK : THEME.LIGHT;
+
+  useEffect(() => setHasMounted(true), []);
+
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <main className="flex flex-1 max-h-screen flex-col items-center">
